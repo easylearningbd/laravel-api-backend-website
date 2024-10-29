bccaa7433f946fa25083636e00e9b77a4ed1ba3c
easylearningbd <elebdsoft@gmail.com>
2024-10-30 02:47:25 +0600

    63 Admin Profile & Image Update Part 1

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -148,41 +148,41 @@
                                 </div>
                             </li>
         
-                            <li class="dropdown notification-list topbar-dropdown">
-                                <a class="nav-link dropdown-toggle nav-user me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                    <img src="{{ asset('backend/assets/images/users/user-11.jpg') }}" alt="user-image" class="rounded-circle">
-                                    <span class="pro-user-name ms-1">
-                                        Christian <i class="mdi mdi-chevron-down"></i> 
-                                    </span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
-                                    <!-- item-->
-                                    <div class="dropdown-header noti-title">
-                                        <h6 class="text-overflow m-0">Welcome !</h6>
-                                    </div>
-        
-                                    <!-- item-->
-                                    <a href="pages-profile.html" class="dropdown-item notify-item">
-                                        <i class="mdi mdi-account-circle-outline fs-16 align-middle"></i>
-                                        <span>My Account</span>
-                                    </a>
-        
-                                    <!-- item-->
-                                    <a href="auth-lock-screen.html" class="dropdown-item notify-item">
-                                        <i class="mdi mdi-lock-outline fs-16 align-middle"></i>
-                                        <span>Lock Screen</span>
-                                    </a>
-        
-                                    <div class="dropdown-divider"></div>
-        
-                                    <!-- item-->
-                                    <a href="{{ route('admin.logout') }}" class="dropdown-item notify-item">
-                                        <i class="mdi mdi-location-exit fs-16 align-middle"></i>
-                                        <span>Logout</span>
-                                    </a>
-        
-                                </div>
-                            </li>
+    <li class="dropdown notification-list topbar-dropdown">
+        <a class="nav-link dropdown-toggle nav-user me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+            <img src="{{ asset('backend/assets/images/users/user-11.jpg') }}" alt="user-image" class="rounded-circle">
+            <span class="pro-user-name ms-1">
+                Christian <i class="mdi mdi-chevron-down"></i> 
+            </span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
+            <!-- item-->
+            <div class="dropdown-header noti-title">
+                <h6 class="text-overflow m-0">Welcome !</h6>
+            </div>
+
+            <!-- item-->
+            <a href="{{ route('admin.profile') }}" class="dropdown-item notify-item">
+                <i class="mdi mdi-account-circle-outline fs-16 align-middle"></i>
+                <span>My Account</span>
+            </a>
+
+            <!-- item-->
+            <a href="auth-lock-screen.html" class="dropdown-item notify-item">
+                <i class="mdi mdi-lock-outline fs-16 align-middle"></i>
+                <span>Lock Screen</span>
+            </a>
+
+            <div class="dropdown-divider"></div>
+
+            <!-- item-->
+            <a href="{{ route('admin.logout') }}" class="dropdown-item notify-item">
+                <i class="mdi mdi-location-exit fs-16 align-middle"></i>
+                <span>Logout</span>
+            </a>
+
+        </div>
+    </li>
         
                         </ul>
                     </div>
---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.master')
+@section('admin')
+
+
+
+
+
+@endsection 
