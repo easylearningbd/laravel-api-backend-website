dc4a85e7cf9163919c573d13ef0c52bb750aa53c
easylearningbd <elebdsoft@gmail.com>
2024-10-30 05:01:57 +0600

    69 Adding Toster In For View Message

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -29,19 +29,41 @@ class ProfileController extends Controller
         $data->name = $request->name;
         $data->email = $request->email;
 
+        $oldPhotoPath = $data->image;
+
         if ($request->hasFile('image')) {
             $file = $request->file('image');
             $filename = time().'.'.$file->getClientOriginalExtension(); // 2323.jpg
             $file->move(public_path('upload/user_images'),$filename);
             $data->image = $filename;
+
+            if ($oldPhotoPath && $oldPhotoPath !== $filename) {
+               $this->deleteOldImage($oldPhotoPath);
+            }
+
+
         }
 
         $data->save();
-        return redirect()->back();
+
+        $notification = array(
+            'message' => 'Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+
+        return redirect()->back()->with($notification);
 
      }
      // End Method
 
+     private function deleteOldImage(string $oldPhotoPath): void {
+        $fullPath = public_path('upload/user_images/'.$oldPhotoPath);
+        if (file_exists($fullPath)) {
+            unlink($fullPath);
+        }
+     }
+        // End private Method
 
 
 
