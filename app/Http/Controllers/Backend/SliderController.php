59005a3d5b10a7ece404512881dfb64ef6331c1a
easylearningbd <elebdsoft@gmail.com>
2024-10-31 00:33:28 +0600

    73 Admin Backend Slider Option Setup Part 2

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Slider;
 
 class SliderController extends Controller
 {
     public function AllSlider(){
-
+    	$slider = Slider::latest()->get();
+    	return view('backend.slider.all_slider',compact('slider'));
     }
     // End Method 
-    
+
 
 
 }
