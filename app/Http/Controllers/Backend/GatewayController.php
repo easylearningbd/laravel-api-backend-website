31b908c81f629ae368dd56af4663aac4be606eca
easylearningbd <elebdsoft@gmail.com>
2024-10-31 16:43:47 +0600

    87 Backend Payment Getaway Services  Setup Part 2

--- app/Http/Controllers/Backend/GatewayController.php
+++ app/Http/Controllers/Backend/GatewayController.php
@@ -16,6 +16,54 @@ class GatewayController extends Controller
     }
     // End Method 
 
+     public function UpdateGetWayOne(Request $request){
+    	$getone_id = $request->id;
+    	$getone = Gatewayone::find($getone_id);
+
+    	if ($request->file('image')) {
+		$image = $request->file('image');
+		$manager = new ImageManager(new Driver());
+		$name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension(); 
+		$img = $manager->read($image);
+		$img->resize(960,679)->save(public_path('upload/gateway/'.$name_gen));
+		$save_url = 'upload/gateway/'.$name_gen;
+
+		if (file_exists(public_path($getone->image))) {
+			@unlink(public_path($getone->image));
+		}
+
+		$getone->update([
+			'title' => $request->title,
+			'description' => $request->description,
+			'phone' => $request->phone,
+			'image' => $save_url,
+		]);
+
+		$notification = array(
+            'message' => 'Updated with Image Successfully',
+            'alert-type' => 'success'
+        ); 
+        return redirect()->back()->with($notification);
+
+	  } else {
+
+	  	$getone->update([
+			'title' => $request->title,
+			'description' => $request->description,
+			'phone' => $request->phone, 
+		]);
+
+		$notification = array(
+            'message' => 'Updated with Image Successfully',
+            'alert-type' => 'success'
+        ); 
+        return redirect()->back()->with($notification);
+
+	  } 
+
+    }
+     // End Method
+
 
 
 } 
