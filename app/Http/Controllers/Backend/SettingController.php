3f5bb3b80e451e1e429633a84f75e76eae51537d
easylearningbd <elebdsoft@gmail.com>
2024-11-01 17:35:38 +0600

    103 Admin Site Setting Setup Part 2

--- app/Http/Controllers/Backend/SettingController.php
+++ app/Http/Controllers/Backend/SettingController.php
@@ -14,6 +14,29 @@ class SettingController extends Controller
     }
     // End Method 
 
+     public function UpdateSiteSetting(Request $request){
+    	$setting_id = $request->id;
+    	$setting = SiteSetting::find($setting_id);
+
+    	$setting->update([
+			'email' => $request->email,
+			'phone' => $request->phone,
+			'facebook' => $request->facebook,
+			'youtube' => $request->youtube,
+			'footer_message' => $request->footer_message,
+			'address' => $request->address,
+		]);
+
+		$notification = array(
+            'message' => 'Setting Updated Successfully',
+            'alert-type' => 'success'
+        ); 
+        return redirect()->back()->with($notification);
+
+    	 }
+    // End Method 
+
+
 
 
 
